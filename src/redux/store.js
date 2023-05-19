3b7cbc4124e94509a5084b6049d7406448c03906
Kevin Sasse <sassek70@gmail.com>
2023-05-19 00:16:59 -0400

    reducer sort of works

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,42 @@
-import { createStoreHook as createStore} from "react-redux";
+// import { createStoreHook as createStore} from "react-redux";
 
-const heroState = {
-    hero = {
-        UserId : 
-        HeroName: 
-        Class: 
-        HeroLevel: 
-        TotalHealth: 
-        CurrentHealth: 
-        TotalAttack: 
-        TotalDefense: 
-        TotalBattles: 
-        BattlesWon: 
-        Deaths: 
-        Coins: 
-        TotalEquippedWeaponSize: 
-        TotalEquippedArmorSize: 
-        EquippedWeaponId1: 
-        EquippedWeaponId2: 
-        EquippedArmorId1: 
-        EquippedArmorId2: 
-        EquippedArmorId3: 
-        CurrentZone: 
-        CurrentNode: 
-        DateLastPlayed: 
-    }
+// const heroState = {
+//         UserId : 2,
+//         HeroName: 2,
+//         Class: 2,
+//         HeroLevel: 2,
+//         TotalHealth: 2,
+//         CurrentHealth: 2,
+//         TotalAttack: 2,
+//         TotalDefense: 2,
+//         TotalBattles: 2,
+//         BattlesWon: 2,
+//         Deaths: 2,
+//         Coins: 2,
+//         TotalEquippedWeaponSize: 2,
+//         TotalEquippedArmorSize: 2,
+//         EquippedWeaponId1: 2,
+//         EquippedWeaponId2: 2,
+//         EquippedArmorId1: 2,
+//         EquippedArmorId2: 2,
+//         EquippedArmorId3: 2,
+//         CurrentZone: 2,
+//         CurrentNode: 2,
+//         DateLastPlayed: 2
+// }
 
-}
+// const reducer = (state = heroState, action) => {
+//     switch(action.type) {
+//         case "updateHeroHealth":
+//             return {...state, CurrentHealth: action.payload}
+//         case "test":
+//             console.log(action.payload)
+//             break;
+//         default: 
+//             return state;
+//         }
+//     }
 
-const reducer = (state, action) {
-    switch(action.type) {
-        case "updateHeroHealth" :
-            ...state,
-            CurrentHealth: action.payload
-        }
-    }
-}
+// const store = createStore(reducer)
 
-const store = createStore(reducer)
+// store.dispatch({ type: "test", payload: "hello"})
